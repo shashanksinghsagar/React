25bb66f9d3b5003f175ebcf69b1dec4376b37748
shashanksinghsagar <shashanksinghsagar5@gmail.com>
2025-05-24 20:10:56 +0530

    02-Adding Card List and Search Box

---
+++ monster-rolodex/src/components/search-box/search-box.component.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react"
+import "./search-box.styles.css"
+
+class SearchBox extends Component {
+  render() {
+    return (
+      <input
+      className={`search-box ${this.props.className}`}
+      type='Search Monster'
+      placeholder={this.props.placeholder}
+      onChange={this.props.onChangeHandler}
+      />
+    );
+  }
+}
+
+export default SearchBox;
